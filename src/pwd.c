dd5138da7e3b3eeb8c8dcc5878520dccb9a2628f
ivalimak <ivalimak@student.hive.fi>
2024-01-22 17:31:09 +0200

    Added env and tweaked pwd

--- src/pwd.c
+++ src/pwd.c
@@ -6,7 +6,7 @@
 /*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 15:33:01 by ivalimak          #+#    #+#             */
-/*   Updated: 2024/01/22 15:38:02 by ivalimak         ###   ########.fr       */
+/*   Updated: 2024/01/22 17:25:18 by ivalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	msh_pwd(void)
 {
 	char	*path;
 
-	path = getenv("PWD");
+	path = msh_getenv("PWD");
 	if (!path)
 		return (1);
 	ft_putstr_fd(path, 1);
