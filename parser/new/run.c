a6439c5c36ffe2012a13e3f603086ac8653afc45
david <dhorvath@student.hive.fi>
2024-02-05 21:11:03 +0200

    tree execution added

--- parser/new/run.c
+++ parser/new/run.c
@@ -6,13 +6,13 @@
 /*   By: dhorvath <dhorvath@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 10:57:40 by dhorvath          #+#    #+#             */
-/*   Updated: 2024/02/05 16:06:14 by dhorvath         ###   ########.fr       */
+/*   Updated: 2024/02/05 16:08:26 by dhorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-int	run_tree(t_node tree)
+static int	run_tree(t_node tree)
 {
 	int	exitcode;
 
@@ -21,14 +21,14 @@ int	run_tree(t_node tree)
 		exitcode = run_tree(tree.left);
 		if (ft_strncmp(tree.content, "||", 2) == 0)
 		{
-			if (exitcode != 0)
+			if (exitcode)
 				return (run_tree(tree.right));
 			else
 				return (exitcode);
 		}
 		else if (ft_strncmp(tree.content, "&&", 2) == 0)
 		{
-			if (exitcode == 0)
+			if (!exitcode)
 				return (run_tree(tree.right));
 			else
 				return (exitcode);
