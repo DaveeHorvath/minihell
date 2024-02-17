2e9ec401e9b342c5362c4c546ad82d15320fef04
david <dhorvath@student.hive.fi>
2024-02-17 19:51:10 +0200

    error prototypes added to header

--- parser/errors.c
+++ parser/errors.c
@@ -6,18 +6,17 @@
 /*   By: dhorvath <dhorvath@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 22:50:18 by dhorvath          #+#    #+#             */
-/*   Updated: 2024/02/05 22:54:01 by dhorvath         ###   ########.fr       */
+/*   Updated: 2024/02/17 17:15:16 by dhorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft/libft.h"
 #include "parser.h"
 #include <string.h>
 
-void	cmd_not_found(char *cmd)
+void	cmd_not_found(void)
 {
 	ft_putstr_fd("command not found: ", 1);
-	ft_putstr_fd(cmd, 1);
 	exit(127);
 }
 
