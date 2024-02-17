d3237c8bcd353fd4f57e393c0ea15eac72060e00
david <dhorvath@student.hive.fi>
2024-02-17 19:48:01 +0200

    testing file for later updates

--- parser/test.c
+++ parser/test.c
@@ -6,15 +6,29 @@
 /*   By: dhorvath <dhorvath@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 17:16:22 by dhorvath          #+#    #+#             */
-/*   Updated: 2024/02/16 20:18:09 by dhorvath         ###   ########.fr       */
+/*   Updated: 2024/02/17 19:13:03 by dhorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include <stdio.h>
+#include <fcntl.h>
 
-int	main(void)
+// int	main(int argc, char **argv, char **env)
+// {
+// 	char *arg[] = {"wc", "-l", NULL};
+// 	int	fd_out = open("Makefile", O_RDONLY);
+// 	int status;
+// 	(void) argc; (void) argv;
+// 	//execute_string("echo 2 | cat <a >c echo 1 | ls | wc -l | echo $PATH | clear)");
+// 	t_cmd cmd = (t_cmd){.argv=arg, .env=env, .fd={fd_out, 1}};
+// 	do_cmd(&cmd);
+// 	waitpid(cmd.pid, &status, 1);
+// 	close(fd_out);
+// }
+
+
+int	main(int argc, char **argv, char **env)
 {
-	get_command(" >a teasdst >>     test \'<a\' >b   <<   \"ad\"  testststatas  testasd ");
-	//printf("%s\n", cmd->argv[0]);
+	exec_pipeline("ls | wc");
 }
