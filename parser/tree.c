5fc3a677caf4ad860947e96612ed327674f17349
david <dhorvath@student.hive.fi>
2024-02-17 19:50:45 +0200

    print tree function for debug

--- parser/tree.c
+++ parser/tree.c
@@ -6,21 +6,21 @@
 /*   By: dhorvath <dhorvath@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 12:05:19 by dhorvath          #+#    #+#             */
-/*   Updated: 2024/02/05 21:11:55 by dhorvath         ###   ########.fr       */
+/*   Updated: 2024/02/17 18:22:54 by dhorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
-#include "libft.h"
+#include "../libft/libft.h"
 
 static char	*trim_parenthesies(char *s)
 {
 	int	start;
 	int	end;
 
-	start = ft_strchr(s, '(') - s;
+	start = ft_strchr(s, '(') - s + 1;
 	end = ft_strrchr(s, ')') - s;
-	return (ft_substr(s, start, end - start));
+	return (ft_push(ft_substr(s, start, end - start)));
 }
 
 static int	need_parenthesies_remove(char *s)
@@ -79,27 +79,28 @@ static int	get_separator_index(char *s)
 	return (need_parenthesies_remove(s));
 }
 
-t_node	make_tree(char *s)
+t_node	*make_tree(char *s)
 {
-	t_node	out;
+	t_node	*out;
 	int		separator_index;
 
+	// printf("%s\n", s);
+	out = ft_push(ft_alloc(sizeof(t_node)));
 	separator_index = get_separator_index(s);
 	if (separator_index == -2)
 		return (make_tree(trim_parenthesies(s)));
 	else if (separator_index == -1)
-		return ((t_node){.left = NULL, .right = NULL, .content = s});
+	{
+		out->left = NULL;
+		out->right = NULL;
+		out->content = s;
+		return (out);
+	}
 	else
 	{
-		out.content = ft_push(ft_substr(s, separator_index, 2));
-		if (!out.content)
-		{
-			out.left = NULL;
-			out.right = NULL;
-			return (out);
-		}
-		out.left = make_tree(ft_push(ft_substr(s, 0, separator_index)));
-		out.right = make_tree(ft_push(ft_substr(s, separator_index + 2,
+		out->content = ft_push(ft_substr(s, separator_index, 2));
+		out->left = make_tree(ft_push(ft_substr(s, 0, separator_index)));
+		out->right = make_tree(ft_push(ft_substr(s, separator_index + 2,
 						ft_strlen(s))));
 		return (out);
 	}
