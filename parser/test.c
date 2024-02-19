3fbea26a2439f05862348d7f09ea30a5540e9bb0
david <dhorvath@student.hive.fi>
2024-02-19 21:57:04 +0200

    new tests added

--- parser/test.c
+++ parser/test.c
@@ -6,7 +6,7 @@
 /*   By: dhorvath <dhorvath@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 17:16:22 by dhorvath          #+#    #+#             */
-/*   Updated: 2024/02/17 19:13:03 by dhorvath         ###   ########.fr       */
+/*   Updated: 2024/02/19 21:44:54 by dhorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 // 	int	fd_out = open("Makefile", O_RDONLY);
 // 	int status;
 // 	(void) argc; (void) argv;
-// 	//execute_string("echo 2 | cat <a >c echo 1 | ls | wc -l | echo $PATH | clear)");
+// 	execute_string("echo 2 | cat <a >c echo 1 | ls | wc -l | echo $PATH | clear)");
 // 	t_cmd cmd = (t_cmd){.argv=arg, .env=env, .fd={fd_out, 1}};
 // 	do_cmd(&cmd);
 // 	waitpid(cmd.pid, &status, 1);
@@ -30,5 +30,10 @@
 
 int	main(int argc, char **argv, char **env)
 {
-	exec_pipeline("ls | wc");
+	(void)argc;
+	(void)argv;
+	(void)env;
+
+	execute_string("(echo 1 && echo 2) && echo 3");
+	execute_string("(ls) && echo 2");
 }
