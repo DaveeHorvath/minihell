490c32e9dac4ab7af1d4afd964280b2f700c59c7
david <dhorvath@student.hive.fi>
2024-02-19 21:57:31 +0200

    added back running a tree

--- parser/run.c
+++ parser/run.c
@@ -6,40 +6,40 @@
 /*   By: dhorvath <dhorvath@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 10:57:40 by dhorvath          #+#    #+#             */
-/*   Updated: 2024/02/17 18:17:12 by dhorvath         ###   ########.fr       */
+/*   Updated: 2024/02/19 18:51:58 by dhorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "parser.h"
 
-// static int	run_tree(t_node *tree)
-// {
-// 	int	exitcode;
+static int	run_tree(t_node *tree)
+{
+	int	exitcode;
 
-// 	if (tree->left && tree->right)
-// 	{
-// 		exitcode = run_tree(tree->left);
-// 		if (ft_strncmp(tree->content, "||", 2) == 0)
-// 		{
-// 			if (exitcode)
-// 				return (run_tree(tree->right));
-// 			else
-// 				return (exitcode);
-// 		}
-// 		else if (ft_strncmp(tree->content, "&&", 2) == 0)
-// 		{
-// 			if (!exitcode)
-// 				return (run_tree(tree->right));
-// 			else
-// 				return (exitcode);
-// 		}
-// 		else
-// 			return (-1);
-// 	}
-// 	else
-// 		return (exec_pipeline(tree->content));
-// }
+	if (tree->left && tree->right)
+	{
+		exitcode = run_tree(tree->left);
+		if (ft_strncmp(tree->content, "||", 2) == 0)
+		{
+			if (exitcode)
+				return (run_tree(tree->right));
+			else
+				return (exitcode);
+		}
+		else if (ft_strncmp(tree->content, "&&", 2) == 0)
+		{
+			if (!exitcode)
+				return (run_tree(tree->right));
+			else
+				return (exitcode);
+		}
+		else
+			return (-1);
+	}
+	else
+		return (exec_pipeline(tree->content));
+}
 
 static void	print_tree(t_node *tree, int depth)
 {
@@ -59,6 +59,6 @@ int	execute_string(char *s)
 	//if (!validate_tree(tree))
 	//	return (-1);
 	print_tree(tree, 0);
-	//run_tree(tree);
+	run_tree(tree);
 	return (0);
 }
