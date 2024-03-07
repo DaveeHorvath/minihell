6203600b162f90c212186736b0ef7741c4ff4025
ivalimak <ivalimak@student.hive.fi>
2024-03-07 14:26:08 +0200

    Added --rcfile and --init-file options

--- inc/minish.h
+++ inc/minish.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minish.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dhorvath <dhorvath@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 14:11:28 by ivalimak          #+#    #+#             */
-/*   Updated: 2024/03/02 13:03:47 by dhorvath         ###   ########.fr       */
+/*   Updated: 2024/03/07 14:25:20 by ivalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,22 @@
 # include "parser.h"
 # include "builtins.h"
 # include "ft_readline.h"
+# include <signal.h>
 # define E_MALLOC 1
 # define E_READLINE 2
 # define E_PIPE 3
 # define E_FORK 4
 # define E_DUP 5
+# define E_ARGS 6
 # define E_EXEC 127
+
+typedef struct s_msh_opts
+{
+	const char	*cfg_fname;
+}	t_msh_opts;
 
 // utils.c
 void	msh_quit(int estatus);
 int		msh_return(int rval);
-
-/* signal handling */
-#include <signal.h>
 
 #endif
