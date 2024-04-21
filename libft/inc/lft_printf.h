1e4e7cd52f9e3a135af61c1bfbbf64898b5de033
ilmu23 <ivalimak@student.hive.fi>
2024-04-21 21:22:22 +0300

    Update libft

--- libft/inc/lft_printf.h
+++ libft/inc/lft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 11:36:57 by ivalimak          #+#    #+#             */
-/*   Updated: 2024/04/17 14:19:44 by ivalimak         ###   ########.fr       */
+/*   Updated: 2024/03/14 05:00:15 by ivalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdint.h>
 # include "lft_limits.h"
 
-int64_t	ft_printf(const char *f, ...);
-int64_t	ft_dprintf(int32_t fd, const char *f, ...);
+ssize_t	ft_printf(const char *f, ...);
+ssize_t	ft_dprintf(int32_t fd, const char *f, ...);
 
 #endif
